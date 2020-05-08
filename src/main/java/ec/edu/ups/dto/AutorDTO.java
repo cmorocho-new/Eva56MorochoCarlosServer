b7601b22194cd01546157659419ba3ce0f383f78
Carlos Morocho <47749329+cmorocho-new@users.noreply.github.com>
2020-05-08 16:41:51 -0500

    Cambio modelo Autor

--- src/main/java/ec/edu/ups/dto/AutorDTO.java
+++ src/main/java/ec/edu/ups/dto/AutorDTO.java
@@ -22,10 +22,25 @@ public class AutorDTO extends GeneralDTO implements Serializable {
 	@Enumerated(EnumType.STRING)
 	private Genero genero;
 	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+
 	@Override
 	public String toString() {
 		return nombre;
 	}
-
-
 }
